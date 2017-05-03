7e5978d5e9a16ad92370c600d8a320aca4fc8424
Glenn Reyes <glenn@glennreyes.com>
2017-05-03 16:16:40 +0200

    Copy paste is so bad

--- packages/2017-05-19-leveraging-code-splitting-in-react-apps/webpack.config.production.js
+++ packages/2017-05-19-leveraging-code-splitting-in-react-apps/webpack.config.production.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/talks/packages/2017-04-27-react-kyiv-rise-of-low-configuration-tooling/dist/"
+    publicPath: "/talks/packages/2017-05-19-leveraging-code-splitting-in-react-apps/dist/"
   },
   plugins: [
     new webpack.DefinePlugin({
